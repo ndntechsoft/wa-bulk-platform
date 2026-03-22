975ea6dc550cdf2004e10032e499a8074850209f
ndntechsoft <ndntechsoft@gmail.com>
2026-03-22 10:00:20 +0530

    Update sender.js

--- sender/sender.js
+++ sender/sender.js
@@ -1,1 +1,66 @@
 
+const axios = require("axios")
+const fs = require("fs")
+const csv = require("csv-parser")
+
+const TOKEN = process.env.TOKEN
+const PHONE_ID = process.env.PHONE_ID
+
+const TEMPLATE = "offer_waterpark"
+
+let numbers=[]
+
+fs.createReadStream("numbers.csv")
+.pipe(csv())
+.on("data",(row)=>{
+
+numbers.push(row.number)
+
+})
+.on("end",()=>{
+
+send()
+
+})
+
+async function send(){
+
+for(const phone of numbers){
+
+try{
+
+await axios.post(
+`https://graph.facebook.com/v18.0/${PHONE_ID}/messages`,
+{
+messaging_product:"whatsapp",
+to:phone,
+type:"template",
+template:{
+name:TEMPLATE,
+language:{code:"en_US"}
+}
+},
+{
+headers:{
+Authorization:`Bearer ${TOKEN}`
+}
+}
+)
+
+fs.appendFileSync("sent.csv",phone+"\n")
+
+console.log("Sent",phone)
+
+}catch(e){
+
+fs.appendFileSync("failed.csv",phone+"\n")
+
+console.log("Failed",phone)
+
+}
+
+await new Promise(r=>setTimeout(r,7000))
+
+}
+
+}
